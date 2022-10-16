91b941a70f75b4681e554066242465006f15d7f0
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2022-10-16 17:31:24 -0400

    Added two section in the dashboard page

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 // Start the welcome count
-let projectCount = document.querySelector(".project-count");
-let moneyCount = document.querySelector(".money-count");
+// let projectCount = document.querySelector(".project-count");
+// let moneyCount = document.querySelector(".money-count");
 
-console.log(projectCount.innerHTML, moneyCount.innerHTML);
-console.log(projectCount.dataset.count, moneyCount.dataset.count);
+// setInterval(() => {
+//     if (projectCount.innerHTML !== projectCount.dataset.count) {
+//         projectCount.innerHTML++;
+//     }
+// }, 2000 / projectCount.dataset.count);
 
-setInterval(() => {
-    if (projectCount.innerHTML !== projectCount.dataset.count) {
-        projectCount.innerHTML++;
-    }
-}, 2000 / projectCount.dataset.count);
-
-setInterval(() => {
-    if (moneyCount.innerHTML !== moneyCount.dataset.count) {
-        moneyCount.innerHTML++;
-    }
-}, 200 / moneyCount.dataset.count);
+// setInterval(() => {
+//     if (moneyCount.innerHTML !== moneyCount.dataset.count) {
+//         moneyCount.innerHTML++;
+//     }
+// }, 200 / moneyCount.dataset.count);
 
 // End the welcome count
+/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
