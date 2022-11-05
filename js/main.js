2d3b3553d433d7b64279bda132c2a9125de48e4c
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2022-11-05 15:41:17 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 // Start the welcome count
-// let projectCount = document.querySelector(".project-count");
-// let moneyCount = document.querySelector(".money-count");
+let projectCount = document.querySelector(".project-count");
+let moneyCount = document.querySelector(".money-count");
 
-// setInterval(() => {
-//     if (projectCount.innerHTML !== projectCount.dataset.count) {
-//         projectCount.innerHTML++;
-//     }
-// }, 2000 / projectCount.dataset.count);
+setInterval(() => {
+    if (projectCount.innerHTML !== projectCount.dataset.count) {
+        projectCount.innerHTML++;
+    }
+}, 2000 / projectCount.dataset.count);
 
-// setInterval(() => {
-//     if (moneyCount.innerHTML !== moneyCount.dataset.count) {
-//         moneyCount.innerHTML++;
-//     }
-// }, 200 / moneyCount.dataset.count);
+setInterval(() => {
+    if (moneyCount.innerHTML !== moneyCount.dataset.count) {
+        moneyCount.innerHTML++;
+    }
+}, 200 / moneyCount.dataset.count);
 
 // End the welcome count
 /* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
